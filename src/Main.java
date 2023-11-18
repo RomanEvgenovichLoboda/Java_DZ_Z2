8093e4bfe885530498b82f973fc89decb01c113d
RomanEvgenovichLoboda <loboda.rom@gmail.com>
2023-11-18 18:54:31 +0200

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
+//Программа запрашивает шестизначное число, после ввода определяет будет ли являтся
+//счастливым билет с таким номером (сумма первых трех цифр совпадает с сумой трех последних).
+//Пример входных данных: 421151; 954832
+//Вывод: Да; Нет
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.print("Введіть шестизначне число:\t");
+        Scanner scanner = new Scanner(System.in);
+        String numberStr = scanner.nextLine();
+        char[] numbers = numberStr.toCharArray();
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int i = 0; i < numbers.length/2; i++) {
+            sum1 += Integer.parseInt(String.valueOf(numbers[i]));
+            sum2 += Integer.parseInt(String.valueOf(numbers[i + numbers.length/2]));
         }
+        System.out.print(sum1 == sum2?"\tЩасливе число":"\tЧисло не є щасливим");
     }
 }
